db944b5645355e39e0a727d75f641494cbf76281
mtsgi <oq@live.jp>
2019-02-17 08:38:25 +0900

    gradParseメソッドを作った

--- gradator.js
+++ gradator.js
@@ -13,12 +13,7 @@ function Load() {
     $(document).on("change", "input", update);
     if( localStorage["gradator-preset"] ){
         preset[0] = JSON.parse( localStorage["gradator-preset"] );
-        var code = "linear-gradient(";
-        for( i in preset[0] ){
-            code += "#" + preset[0][i] + " " + i + "%,";
-        }
-        code = code.substr( 0, code.length-1 ) + ")";
-        $("#p0").css("background", code);
+        $("#p0").css("background", gradParse(preset[0]));
     }
 }
 
@@ -30,25 +25,21 @@ function update(){
         let color = $(".colorname")[i].value;
         grad[String(point)] = color;
     }
-    var code = "linear-gradient(";
-    for( i in grad ){
-        code += "#" + grad[i] + " " + i + "%,";
-    }
-    code = code.substr( 0, code.length-1 ) + ")";
-    $("#colorbox").css("background", code);
-    $("#code").text("background : " + code + ";");
+    $("#colorbox").css("background", gradParse(grad));
+    $("#code").text("background : " + gradParse(grad) + ";");
 }
 
 function readPreset( _preset ){
     console.log(_preset);
     grad = _preset;
-    var code = "linear-gradient(";
+    $("#stops").html("");
     for( i in grad ){
-        code += "#" + grad[i] + " " + i + "%,";
+        $("#stops").append("<div class='colorstop' id='stopbase'><span>位置(%)</span><input type='number' min='0' max='100' class='point' value='"+i+"'><span>カラー</span><input type='text' class='colorname jscolor' value='"+grad[i]+"'></div>");
+        new jscolor( $(".colorname")[ $(".colorname").length-1 ] );
     }
-    code = code.substr( 0, code.length-1 ) + ")";
-    $("#colorbox").css("background", code);
-    $("#code").text("background : " + code + ";");
+    $("#colorbox").css("background", gradParse(grad));
+    $("#code").text("background : " + gradParse(grad) + ";");
+    update();
 }
 
 function addStop(){
@@ -77,4 +68,13 @@ function addPreset(){
     }
     code = code.substr( 0, code.length-1 ) + ")";
     $("#p0").css("background", code);
+}
+
+function gradParse( _obj ){
+    var code = "linear-gradient(";
+    for( i in _obj ){
+        code += "#" + _obj[i] + " " + i + "%,";
+    }
+    code = code.substr( 0, code.length-1 ) + ")";
+    return code;
 }
