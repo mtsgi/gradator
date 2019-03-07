c1b11f620e399c05184d87ac88184fe8f8b9920e
mtsgi <oq@live.jp>
2019-03-07 22:27:02 +0900

    ユーザープリセット用の準備

--- gradator.js
+++ gradator.js
@@ -1,14 +1,15 @@
 $( document ).ready( Load );
 
 var grad = new Object();
+var user = new Array();
 var preset = new Array();
 const stopbase = "<div class='colorstop' id='stopbase'><span>位置(%)</span><input type='number' min='0' max='100' class='point' value='0'><span>カラー</span><input type='text' class='colorname jscolor'></div>";
 
-preset[1] = {"0":"16D5F7","100":"125CE6"}
-preset[2] = {"0":"8A8A8A","48":"454545","52":"000000","100":"474747"}
-preset[3] = {"0":"FF0000","20":"FFFF00","40":"00FF00","60":"00FFFF","80":"0000FF","100":"FF00FF"}
-preset[4] = {"48":"21EB07","52":"11D100"};
-preset[5] = {"0":"E0E0E0","100":"FFFFFF"};
+preset[0] = {"0":"16D5F7","100":"125CE6"}
+preset[1] = {"0":"8A8A8A","48":"454545","52":"000000","100":"474747"}
+preset[2] = {"0":"FF0000","20":"FFFF00","40":"00FF00","60":"00FFFF","80":"0000FF","100":"FF00FF"}
+preset[3] = {"48":"21EB07","52":"11D100"};
+preset[4] = {"0":"E0E0E0","100":"FFFFFF"};
 
 function Load() {
     $(".point")[0].value = "0";
@@ -18,8 +19,13 @@ function Load() {
     update();
     $(document).on("change", "input", update);
     let cnt = 0;
+    if( localStorage["gradator-preset"] ){
+        let userpreset = JSON.parse( localStorage["gradator-preset"] );
+        for( i in userpreset ){
+            user[i] = gradParse( userpreset[i] );
+        }
+    }
     for( i of $(".preset") ){
-        if( localStorage["gradator-preset"] ) preset[0] = JSON.parse( localStorage["gradator-preset"] );
         $(i).css("background", gradParse(preset[cnt]));
         cnt ++;
     }
@@ -66,15 +72,15 @@ function copy2cb(){
     $("#done").show().fadeOut(800);
 }
 
-function addPreset(){
+function addPreset(index){
     localStorage.setItem("gradator-preset", JSON.stringify(grad));
-    preset[0] = JSON.parse( localStorage["gradator-preset"] );
+    preset[index] = JSON.parse( localStorage["gradator-preset"] );
     var code = "linear-gradient(";
-    for( i in preset[0] ){
-        code += "#" + preset[0][i] + " " + i + "%,";
+    for( i in preset[index] ){
+        code += "#" + preset[index][i] + " " + i + "%,";
     }
     code = code.substr( 0, code.length-1 ) + ")";
-    $("#p0").css("background", code);
+    $("#u"+index).css("background", code);
 }
 
 function gradParse( _obj ){
